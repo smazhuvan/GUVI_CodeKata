498c491b7ea9750b7f50607da6d5f492650a0bf6
Shivakumar Mazhuvan <54847304+smazhuvan@users.noreply.github.com>
2020-10-14 01:29:20 +0530

    Update duplicateRemoval.js

--- 07_10_2020/duplicateRemoval.js
+++ 07_10_2020/duplicateRemoval.js
@@ -17,25 +17,14 @@ Sample Output :
 
 // Solution
 
-// Getting input via STDIN
-const readline = require("readline");
 
-const inp = readline.createInterface({
-  input: process.stdin
-});
 
-const userInput = [];
+var inpArr = userInput[0].split("");
 
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-inp.on("close", () => {
- var arr = userInput[0].split("")
-for (i=0; i<arr.length; i++){
-    if (arr[i]===arr[i+1]){
-        arr.splice(i,2)
+for(i=0; i<inpArr.length; i++){
+    if (inpArr[i] === inpArr[i+1]){
+        inpArr.splice(i,2);
     }
 }
-console.log(arr.join(""))
+console.log(inpArr.join(""));
 });
