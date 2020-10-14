0b4495162266c86a542fd75bcf6aba75ff30a5e2
Shivakumar Mazhuvan <54847304+smazhuvan@users.noreply.github.com>
2020-10-14 12:36:13 +0530

    Update simpleInterest.js

--- 10_10_2020/simpleInterest.js
+++ 10_10_2020/simpleInterest.js
@@ -1,22 +1,7 @@
-const readline = require("readline");
-const userInput = [];
+var a = userInput[0].split(" ");
+var princAmount = a[0];
+var rate = a[1];
+var year = a[2];
+var simInt = ((+princAmount * +rate * +year)/100);
 
-const inp = readline.createInterface({
-    input: process.stdin
-});
-
-inp.on ("line",(data) => {
-   userInput.push(data); 
-});
-
-inp.on("close",() => {
-   
-   var a = userInput[0].split(" ");
-   var princAmount = a[0];
-   var rate = a[1];
-   var year = a[2];
-   
-   var simInt = ((+princAmount * +rate * +year)/100);
-   
-   console.log(simInt.toFixed(2));
-});
+console.log(simInt.toFixed(2));
