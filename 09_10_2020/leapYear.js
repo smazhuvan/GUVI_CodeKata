24d56c1d43b9ac9c68d3ceb4eda7ef4db31805f3
Shivakumar Mazhuvan <54847304+smazhuvan@users.noreply.github.com>
2020-10-14 12:27:29 +0530

    Update leapYear.js

--- 09_10_2020/leapYear.js
+++ 09_10_2020/leapYear.js
@@ -17,27 +17,9 @@ Y
 
 */
 
-// Getting input via STDIN
-const readline = require("readline");
-
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-const userInput = [];
-
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-inp.on("close", () => {
+var year = userInput[0];
   
-  var year = userInput[0];
-  
-  if(year % 400 == 0 || year % 100 == 0 || year % 4 == 0){
-      console.log("Y");
-  }
-  else{
-      console.log("N");
-  }
-});
+if(year % 400 == 0 || year % 100 == 0 || year % 4 == 0)
+  console.log("Y");
+else
+  console.log("N");
