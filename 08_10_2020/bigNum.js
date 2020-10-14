96316876aed66a8a0e5dc24f288297a4516ceaf7
Shivakumar Mazhuvan <54847304+smazhuvan@users.noreply.github.com>
2020-10-14 12:21:32 +0530

    Update bigNum.js

--- 08_10_2020/bigNum.js
+++ 08_10_2020/bigNum.js
@@ -17,36 +17,13 @@ Sample Output :
 
 */
 
-// Getting input via STDIN
-const readline = require("readline");
-
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-const userInput = [];
-
-inp.on("line", (data) => {
-  //console.log(data);
-  userInput.push(data);
-});
-inp.on("close", () => {
+var ip1 = userInput[0].split(" ");
+var ip2 = userInput[1].split(" ");
+var ip3 = userInput[2].split(" ");
     
-    var ip1 = userInput[0].split(" ");
-    var ip2 = userInput[1].split(" ");
-    var ip3 = userInput[2].split(" ");
-    
-    if(ip1 > ip2 && ip1 > ip3)
-    {
-        console.log(parseInt(ip1));
-    }
-    else if(ip2 > ip1 && ip2 > ip3)
-    {
-        console.log(parseInt(ip2));
-    }
-    else
-    {
-        console.log(parseInt(ip3));
-    }
-    
-});
+if(ip1 > ip2 && ip1 > ip3)
+  console.log(parseInt(ip1));
+else if(ip2 > ip1 && ip2 > ip3)
+  console.log(parseInt(ip2));
+else
+  console.log(parseInt(ip3));
