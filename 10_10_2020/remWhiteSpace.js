7ce05cc6ca22a9792a59dee01a3b3c6681a6f864
Shivakumar Mazhuvan <54847304+smazhuvan@users.noreply.github.com>
2020-10-14 12:35:37 +0530

    Update remWhiteSpace.js

--- 10_10_2020/remWhiteSpace.js
+++ 10_10_2020/remWhiteSpace.js
@@ -14,24 +14,6 @@ Sample Output :
 10
 
 */
-
-// Getting input via STDIN
-const readline = require("readline");
-
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-const userInput = [];
-
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-inp.on("close", () => {
-    
-  var str = userInput[0].trim();
-  str = str.replace(/\s/g,''); //Regular Expression
-  console.log(str.length);
-  
-});
+var str = userInput[0].trim();
+str = str.replace(/\s/g,''); //Regular Expression
+console.log(str.length);
