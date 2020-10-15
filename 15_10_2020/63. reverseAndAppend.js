2c104aa193a46dc05a78ef6cbd18090409505147
Shivakumar Mazhuvan <54847304+smazhuvan@users.noreply.github.com>
2020-10-16 01:48:37 +0530

    Create 63. reverseAndAppend.js

---
+++ 15_10_2020/63. reverseAndAppend.js
@@ -0,0 +1,12 @@
+/*
+Given a input string S, reverse the given string by appending each character of the string with '-'.
+Input Size : |S| <= 100000
+Sample Testcase :
+INPUT
+codekata
+OUTPUT
+a-t-a-k-e-d-o-c
+*/
+
+var str = userInput[0].split("").reverse().join("-");
+console.log(str);
